04dd8265343d2da7bdfbbf9f2e4a685475f1d6e4
ClasiCKGit <losmellipizzico1@gmail.com>
2026-01-11 16:53:35 -0300

    todoList: add persistencia en localStorage y filtros

--- ToDoList/src/components/todoList.tsx
+++ ToDoList/src/components/todoList.tsx
@@ -1,25 +1,44 @@
-
-import { Todo } from '../types/todo'
-import { TodoItem } from './todoItem'
+import { Filter } from "../todoApp";
+import { Todo } from "../types/todo";
+import { TodoItem } from "./todoItem";
 
 type Props = {
-    todos: Todo[]
-    onToggle: (id: number) => void
-    onDelete: (id: number) => void
-}
+    filter: Filter;
+    todos: Todo[];
+    onToggle: (id: number) => void;
+    onDelete: (id: number) => void;
+};
 
-export const TodoList = ({todos, onToggle, onDelete}: Props) => {
+export const TodoList = ({ filter, todos, onToggle, onDelete }: Props) => {
+    const filteredTodos = todos.filter((todo) => {
+        if (filter === Filter.Completadas) {
+            return todo.completed;
+        }
+        if (filter === Filter.Pendientes) {
+            return !todo.completed;
+        }
+        return true; //Todas las tareas
+    });
     return (
-        <div className='todoList'>
-            { todos.length === 0 
-            ? <span> Por el momento no hay nada para hacer</span>
-            : todos.map(todo=>(
-                <TodoItem 
-                todo={todo}
-                onToggle={onToggle}
-                onDelete={onDelete}
-                />
-            ))}
+        <div className="todoList">
+            {filteredTodos.length === 0 ? (
+                <span>
+                    Por el momento no hay tareas{" "}
+                    {filter === 0
+                        ? "completadas"
+                        : filter === 1
+                        ? "pendientes"
+                        : ""}
+                </span>
+            ) : (
+                filteredTodos.map((todo) => (
+                    <TodoItem
+                        todo={todo}
+                        onToggle={onToggle}
+                        onDelete={onDelete}
+                    />
+                ))
+            )}
         </div>
-    )
-}
+    );
+};
--- ToDoList/src/todoApp.tsx
+++ ToDoList/src/todoApp.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Todo } from "./types/todo";
 import { TodoList } from "./components/todoList";
 import { TodoForm } from "./components/todoForm";
 
+export enum Filter {
+    Completadas,
+    Pendientes,
+    Todas,
+}
+
 export const TodoApp = () => {
-    const [todos, setTodos] = useState<Todo[]>([]);
+    const [todos, setTodos] = useState<Todo[]>(()=>{
+        const data = localStorage.getItem("todos")      //Obtener Todos si hay en el localSotrage
+        return data ? JSON.parse(data) : []             //Si no hay, el estado "todos" se inicializa como []
+    });
+    const [filter, setFilter] = useState<Filter>(Filter.Todas);
 
     const addTodo = (title: string) => {
         const newTodo: Todo = {
@@ -27,14 +37,38 @@ export const TodoApp = () => {
         setTodos((prev) => prev.filter((todo) => todo.id !== id));
     };
 
+    useEffect(()=>{     //useEffect: cada vez que se modifica "todos" se ejecuta lo de abajo
+        localStorage.setItem("todos", JSON.stringify(todos))    //se guarda los todos en "todos" del localStorage
+    }, [todos])
     return (
         <>
             <h1>todoApp</h1>
             <TodoForm onAddTodo={addTodo} />
-            <TodoList 
+            <div className="containerBotones">
+                <button
+                    className="btn btn-success"
+                    onClick={() => setFilter(Filter.Completadas)}
+                >
+                    Mostrar solo completadas
+                </button>
+                <button
+                    className="btn btn-danger"
+                    onClick={() => setFilter(Filter.Pendientes)}
+                >
+                    Mostrar solo pendientes
+                </button>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => setFilter(Filter.Todas)}
+                >
+                    Mostrar todas las tareas
+                </button>
+            </div>
+            <TodoList
+                filter={filter}
                 todos={todos}
                 onToggle={toggleTodo}
-                onDelete= {deleteTodo}
+                onDelete={deleteTodo}
             />
         </>
     );
